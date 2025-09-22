48b9d2b5de587844898446c4765fe1238719ae27
Giovanni Sanchez <giosr295@gmail.com>
2025-09-21 18:26:28 -0600

    v. 2.1.1

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
@@ -36,6 +38,13 @@ app.get('/booking', async (req, res) => {
     }
 });
 
+app.post("/api/reservas", (req, res) => {
+    const datos = req.body;
+    console.log("Datos recibidos:", datos);
+
+    // Aquí podrías guardarlos en un JSON temporal, base de datos, etc.
+    res.json({ status: "ok", recibido: datos });
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
